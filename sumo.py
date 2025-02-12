96271c1263426af6b9739f4bc60ed937392410aa
Yael <yael.rodriguez@iteso.mx>
2025-02-12 09:44:49 -0600

    Como objeto

--- sumo.py
+++ sumo.py
@@ -1,49 +1,57 @@
 import subprocess
 import traci
+from os import path
 
 class SumoSimulation():
+
+    CANNOT_START = 0
+    CAN_START = 1
+    CAN_RUN = 2
+
     def __init__(self):
-        pass
+        self.sumoConfiguration = "assets/simulation.sumocfg"
+        self.step = SumoSimulation.CAN_START if path.exists("assets/simulation.sumocfg") else SumoSimulation.CANNOT_START
 
+    def generate_files(self, nodes, edges, random):
+        CMDNetwork = f"netconvert --node-files {nodes} --edge-files {edges} --tls.guess true --output-file=assets/network.net.xml".split()
 
-def generateFiles():
-    CMDNetwork = '''
-    netconvert --node-files assets/nodes.nod.xml --edge-files assets/edges.edg.xml --tls.guess true --output-file=assets/network.net.xml
-    '''.split()
+        CMDTrips = f"python {random} -n assets/network.net.xml -o assets/traffic.trips.xml --fringe-factor 50"
+        CMDTraffic = "duarouter -n assets/network.net.xml -t assets/traffic.trips.xml -o assets/traffic.rou.xml"
 
-    CMDTrips = "python assets/randomTrips.py -n assets/network.net.xml -o assets/traffic.trips.xml --fringe-factor 50"
-    CMDTraffic = "duarouter -n assets/network.net.xml -t assets/traffic.trips.xml -o assets/traffic.rou.xml"
+        subprocess.run(CMDNetwork)
+        subprocess.run(CMDTrips)
+        subprocess.run(CMDTraffic)
+        self.step = SumoSimulation.CAN_RUN
 
-    subprocess.run(CMDNetwork)
-    subprocess.run(CMDTrips)
-    subprocess.run(CMDTraffic)
+    def start_simulation(self, visual):
+        if(self.step < SumoSimulation.CAN_START): return
+        cmd = ["sumo-gui" if visual else "sumo", "-c", "assets/simulation.sumocfg"]
+        traci.start(cmd)
+        self.step = SumoSimulation.CAN_RUN
 
-
+    def run_simulation(self, steps):
+        if(self.step < SumoSimulation.CAN_RUN): return
+        tl = traci.trafficlight.getIDList()
     
-def runSimulation(steps):
-    cmd = ["sumo", "-c", "assets/simulation.sumocfg"]
-    #cmd = ["sumo-gui", "-c", "assets/simulation.sumocfg"]
+        totalEnds = 0
+        totalStarts = 0
+        for _ in range(steps):
+            traci.simulationStep()
+            totalEnds += traci.simulation.getArrivedNumber()
+            totalStarts  += traci.simulation.getDepartedNumber()
 
-    traci.start(cmd)
-    #Pongo acá los semáforos mejor, también como argumento creo
-    
-    totalEnds = 0
-    for _ in range(steps):
-        traci.simulationStep()
-        lights = traci.trafficlight.getIDList()
-        ended = traci.simulation.getArrivedIDList()
-        totalEnds += len(ended)
-    traci.close()
+        traci.close()
 
-    return totalEnds, lights
-
+        return totalEnds, totalStarts, tl
+        
 
 
 if __name__ == "__main__":
+    SUMO = SumoSimulation()
 
-    a,b = runSimulation(500)    #860 es el limite exclusivo que aguanta la simulación  
-    
-    print(f"Arrives = {a}")
-    print(b)
+    #SUMO.generate_files("assets/nodes.nod.xml", "assets/edges.edg.xml", "assets/randomTrips.py")
+    SUMO.start_simulation(False) 
+    ends, start, sema = SUMO.run_simulation(250)
+    print(ends, start, sema)
 
     
