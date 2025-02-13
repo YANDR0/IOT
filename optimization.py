321d4617424635a8851740e6c348fd230ce90fa4
Yael <yael.rodriguez@iteso.mx>
2025-02-12 18:46:57 -0600

    Caos muerte y destrucción

---
+++ optimization.py
@@ -0,0 +1,227 @@
+from math import cos, pi, e
+from random import random, uniform
+from time import time
+
+
+### Test functions ###
+
+def rosenbrock(x, dim):
+  fx = 0
+  for i in range(dim - 1): fx += 100 * pow(x[i + 1] - x[i] * x[i], 2) + pow(1 - x[i], 2)
+  return fx
+
+def rastrigin(x, dim):
+  fx = 0
+  for i in range(dim): fx += x[i] * x[i] - 10 * cos(2 * pi * x[0])
+  return 10 * dim + fx
+
+def styblinski(x, dim):
+  fx = 0
+  for i in range(dim): fx += pow(x[i], 4) - 16 * pow(x[i], 2) + 5 * x[i]
+  return fx / 2
+
+def ackley(x, dim):
+    sum1 = 0
+    sum2 = 0
+    for i in range(dim): 
+        sum1 += x[i]**2
+        sum2 += cos(2*pi*x[i])
+    return -20*pow(e,-0.2*pow(1/dim*sum1,0.5)) - pow(e, 1/dim*sum2) + 20 + e
+
+# -------------------------------------------------------------
+
+### Vector operations
+class VectorOperations:
+    @staticmethod
+    def sumVector(vector1, vector2, substract=False):
+        sign = -1 if substract else 1
+        return [i+(j)*sign for i, j in zip(vector1, vector2)] 
+    
+    @staticmethod
+    def multiplyScalar(vector, num):
+        return [i*num for i in vector]
+    
+    @staticmethod
+    def ramdomRange(low, high):
+        return [uniform(low[i], high[i]) for i in range(len(low))]
+    
+# -------------------------------------------------------------
+
+### Hill Climbing
+class HillClimbing:
+    def __init__(self, func, xLow, xHigh):
+        self.func = func
+        self.xLow = xLow
+        self.xHigh = xHigh
+        self.dim = len(xLow)
+
+    @staticmethod
+    def ranIncrement(Dx):
+        return [-0.5*dx+random()*dx for dx in Dx]
+    
+    def getDx(self, fitness):
+        return [(h - l)*(1-fitness) for l, h in zip(self.xLow, self.xHigh)]
+
+    def optimizeRange(self, n): 
+        x = VectorOperations.ramdomRange(self.xLow, self.xHigh)
+        y = self.func(x, self.dim)
+        Dx = self.getDx(0)
+        dx = HillClimbing.ranIncrement(Dx)
+
+        for _ in range(n):
+            tempX = VectorOperations.sumVector(x, dx)
+            tempY = self.func(tempX, self.dim)
+            if(tempY < y):
+                x = tempX
+                y = tempY
+                Dx = self.getDx(1/(1+y))
+            else:
+                dx = HillClimbing.ranIncrement(Dx)
+
+        return x, y
+
+# -------------------------------------------------------------
+
+### Swarm algoritm
+
+# Variables de particulas o
+class Swarm:
+    #Crear enjambre
+    def __init__(self, func, w, c1, c2, xLow, xHigh):
+        self.BestX = None
+        self.BestY = None
+        self.func = func
+        self.W = w
+        self.C1 = c1
+        self.C2 = c2
+        self.xLow = xLow
+        self.xHigh = xHigh
+        self.dim = len(xLow)
+    #Creación de particula vinculada al enjambre
+    def createParticle(self):
+        return Particle(self)
+        
+# Particulas
+class Particle:
+    # Creación de una partícula en base a un rango y un enjambre
+    def __init__(self, Swarm: Swarm):
+        self.x = VectorOperations.ramdomRange(Swarm.xLow, Swarm.xHigh)
+        self.v = [random()*2-1 for _ in range(Swarm.dim)]
+        self.bestX = self.x
+        self.bestY = Swarm.func(self.x, Swarm.dim)
+
+        # Check if new position is a best
+        if(not Swarm.BestY or self.bestY < Swarm.BestY): 
+            Swarm.BestX = self.x
+            Swarm.BestY = self.bestY
+
+    # String representation
+    def __repr__(self):
+        return f"x: {str(self.x)}, v: {str(self.v)}"
+    
+    # Actualización de la particla
+    def updateParticle(self, Swarm: Swarm):
+        # Obtener valores
+        r1 = random() * Swarm.C1
+        r2 = random() * Swarm.C2
+        currSpeed = self.v
+
+        # Calcular la nueva velocidad y posición
+        for i in range(Swarm.dim):
+            self.v[i] = Swarm.W * self.v[i] + Swarm.C1 * r1 * (self.bestX[i] - self.x[i]) + Swarm.C2 * r2 * (Swarm.BestX[i] - self.x[i])
+            self.x[i] = currSpeed[i] + self.x[i]
+
+        # Revisar si la nueva posición es un mínimo
+        y = Swarm.func(self.x, Swarm.dim)
+        if(y >= self.bestY): return
+        self.bestY = y
+        self.bestX = self.x
+        if(y >= Swarm.BestY): return
+        Swarm.BestY = y
+        Swarm.BestX = self.x
+
+# -------------------------------------------------------------
+
+### Main ###
+def swarm_simulation(func, xLow, xHigh, m, n):
+
+    swarm = Swarm(func, 0.001, 0.1, 0.1, xLow, xHigh)
+    particles = [swarm.createParticle() for _ in range(m)]
+    for _ in range(n):
+        for p in particles:
+            p.updateParticle(swarm)
+
+    return swarm
+
+def hill_simulation(func, xLow, xHigh, n):
+    hill = HillClimbing(func, xLow, xHigh)
+    return hill.optimizeRange(n)
+    
+def randomMin(func, xLow, xHigh, n):
+    yMin = float("inf")
+    dim = len(xLow)
+    xMin = [0 for _ in range(dim)]
+    
+    for i in range(n):
+        x = VectorOperations.ramdomRange(xLow, xHigh)
+        num = func(x, dim)
+        xMin = x if num < yMin else xMin
+        yMin = min(num, yMin)
+
+    return xMin, yMin
+
+###########
+if __name__ == '__main__':
+    xLow = (-5, -5, -5)
+    xHigh = (5, 5, 5)
+
+    #'''
+    print("Random")
+    start = time()
+    x, y = randomMin(rosenbrock, xLow, xHigh, 100_000)
+    print("Rosenbrock", x, y)
+    x, y = randomMin(rastrigin, xLow, xHigh, 100_000)
+    print("Rastrigin", x, y)
+    x, y = randomMin(styblinski, xLow, xHigh, 100_000)
+    print("Styblinski", x, y)
+    x, y = randomMin(ackley, xLow, xHigh, 100_000)
+    print("Ackley", x, y)
+    print("- Time: ", time()-start)
+    #'''
+    
+    
+    #'''
+    print("\nHill Climbing")
+    start = time()
+    x, y = hill_simulation(rosenbrock, xLow, xHigh, 100_000)
+    print("Rosenbrock", x, y)
+    x, y = hill_simulation(rastrigin, xLow, xHigh, 100_000)
+    print("Rastrigin", x, y)
+    x, y = hill_simulation(styblinski, xLow, xHigh, 100_000)
+    print("Styblinski", x, y)
+    x, y = hill_simulation(ackley, xLow, xHigh, 100_000)
+    print("Ackley", x, y)
+    print("- Time: ", time()-start)
+    #'''
+    
+    
+    #'''
+    print("\nSwarm")
+    start = time()
+    s = swarm_simulation(rosenbrock, xLow, xHigh, 100, 1000)
+    print("Rosenbrock", s.BestX, s.BestY)
+    s = swarm_simulation(rastrigin, xLow, xHigh, 100, 1000)
+    print("Rastrigin", s.BestX, s.BestY)
+    s = swarm_simulation(styblinski, xLow, xHigh, 100, 1000)
+    print("Styblinski", s.BestX, s.BestY)
+    s = swarm_simulation(ackley, xLow, xHigh, 100, 1000)
+    print("Ackley", s.BestX, s.BestY)
+    print("- Time: ", time()-start)
+    #'''
+    
+    
+
+    
+    
+
+
--- sumo.py
+++ sumo.py
@@ -1,6 +1,8 @@
 import subprocess
 import traci
+import optimization
 from os import path
+
 
 class SumoSimulation():
 
@@ -8,13 +10,12 @@ class SumoSimulation():
     CAN_START = 1
     CAN_RUN = 2
 
-    def __init__(self):
-        self.sumoConfiguration = "assets/simulation.sumocfg"
-        self.step = SumoSimulation.CAN_START if path.exists("assets/simulation.sumocfg") else SumoSimulation.CANNOT_START
+    def __init__(self, configuration):
+        self.sumoConfiguration = configuration
+        self.step = SumoSimulation.CAN_START if path.exists(self.sumoConfiguration) else SumoSimulation.CANNOT_START
 
     def generate_files(self, nodes, edges, random):
         CMDNetwork = f"netconvert --node-files {nodes} --edge-files {edges} --tls.guess true --output-file=assets/network.net.xml".split()
-
         CMDTrips = f"python {random} -n assets/network.net.xml -o assets/traffic.trips.xml --fringe-factor 50"
         CMDTraffic = "duarouter -n assets/network.net.xml -t assets/traffic.trips.xml -o assets/traffic.rou.xml"
 
@@ -25,14 +26,13 @@ class SumoSimulation():
 
     def start_simulation(self, visual):
         if(self.step < SumoSimulation.CAN_START): return
-        cmd = ["sumo-gui" if visual else "sumo", "-c", "assets/simulation.sumocfg"]
+        cmd = ["sumo-gui" if visual else "sumo", "-c", self.sumoConfiguration]
         traci.start(cmd)
         self.step = SumoSimulation.CAN_RUN
 
     def run_simulation(self, steps):
         if(self.step < SumoSimulation.CAN_RUN): return
-        tl = traci.trafficlight.getIDList()
-    
+        
         totalEnds = 0
         totalStarts = 0
         for _ in range(steps):
@@ -42,16 +42,53 @@ class SumoSimulation():
 
         traci.close()
 
-        return totalEnds, totalStarts, tl
+        return totalEnds/totalStarts
+    
+    def end_simulation(self):
+        traci.close()
+        self.step = SumoSimulation.CAN_START
         
+class LightsFunctions():
+
+    def __init__(self):
+        self.colorToguether = True
+        self.phases = 2
+
+    @staticmethod
+    # x es la lista con los tiempos de todos los semáforos, las opciones son: 
+    # Todos los verdes, rojos y amarillos juntos, 1r, 2r, 3r...
+    # Todo lo de un semáforo junto, verde, rojo, amarillo, verde, rojo, amarillo, ...
+    # O si permitir de 2 a 4, ocupo funciones distintas lastimosamente
+    def all_lights(x):      
+        SUMO = SumoSimulation("assets/simulation.sumocfg")
+        SUMO.start_simulation(False)
+
+        tl = traci.trafficlight.getIDList()
+        for i in range(len(x)):
+            pass
+            #phases
+
+        '''
+        phases_sem1 = [traci.trafficlight.Phase(20, "GrGr"),  
+            traci.trafficlight.Phase(5, "yryr"),   
+            traci.trafficlight.Phase(20, "rGrG"),  
+            traci.trafficlight.Phase(5, "ryry")]
+
+        traci.trafficlight.setProgramLogic("sem1", traci.trafficlight.Logic("prog1", 0, 0, phases_sem1))
+        '''
 
 
-if __name__ == "__main__":
-    SUMO = SumoSimulation()
+def probar():
+    SUMO = SumoSimulation("assets/simulation.sumocfg")
 
     #SUMO.generate_files("assets/nodes.nod.xml", "assets/edges.edg.xml", "assets/randomTrips.py")
     SUMO.start_simulation(False) 
-    ends, start, sema = SUMO.run_simulation(250)
-    print(ends, start, sema)
+
+    ended = SUMO.run_simulation(250)
+    print(ended)
+
+if __name__ == "__main__":
+    probar()
+    
 
     
