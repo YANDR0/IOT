eb4e4ea15d17fe5a00fe33962cd70e1378d8c47a
Yael <yael.rodriguez@iteso.mx>
2025-02-14 22:21:59 -0600

    La faquin maquina de la chamba (nathan)

--- sumo.py
+++ sumo.py
@@ -3,6 +3,7 @@ import traci
 import optimization     ### Es el de Yandro, luego vemos si usamos otro o que
 from os import path
 
+
 # Administra la simulación de SUMO
 class SumoSimulation():
 
@@ -55,11 +56,12 @@ class SumoSimulation():
         traci.close()
         self.step = SumoSimulation.CAN_START
 
+
 # Configurar las funciones para pasarselas a los algoritmos de optimización (f(x))
 class LightsFunctions():
 
-    # Fases del semáforo (La letra dice de que color está)
-    phases = ["GrGr", "yryr", "rGrG", "ryry"]
+    # Fases del semáforo, verde, amarillo 1, 
+    phases = ["GrGr", "rGrG", "yryr", "ryry"]
 
     # Crea administrador de las funciones, steps define los pasos o ciclos de la simulación
     # color si los datos de x están juntos por color y phases cuantos tiempos se dieron (Más detalles en all lights)
@@ -67,6 +69,7 @@ class LightsFunctions():
         self.simulationSteps = steps  #Cantidad de pasos en la simulación
         self.color_toguether = color  #Parámetros están juntos por color o por semáforo
         self.phases = phases     #Cuales ciclos duran igual
+
 
     # Recibe los tiempos y configura los semáforos de manera individual en base a eso
     def all_lights(self, x):      
@@ -77,33 +80,39 @@ class LightsFunctions():
         tl_num = len(tl)
         tl_dict = {id:[] for id in tl}
         
-        for i in range(tl_num*self.phases+1):
+        
+        for i in range(tl_num*self.phases):
             id = tl[i % tl_num if self.color_toguether else (i-1)//self.phases]
 
             #Si fase es 1, todos los colores duran lo mismo
             if(self.phases == 1):
-                tl_dict[id] = [traci.trafficlight.Phase(x[i], p) for p in LightsFunctions.phases]
+                tl_dict[id] = [traci.trafficlight.Phase(x[i], p) for p in LightsFunctions.phases]   # 0, 1, 2 y 3
+                continue
 
+            step = len(tl_dict[id])
             #Si fase es 2, rojo y verde duran lo mismo, ambos amarillos duran lo mismo
             if(self.phases == 2):
-                step = len(tl_dict[id])//2
-                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step]))
-                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step+1]))
+                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step]))    # 0 o 2  
+                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step+1]))  # 1 o 3
+                continue
 
             #Si fase es 3, cada color dura lo suyo, verde, rojo y ambos amarillos
-            if(self.phases == 3):
-                pass
+            if(step == 2 and self.phases == 3): 
+                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step+1]))  # 3 si phase == 3
 
-            #Si fase es 4, cada etapa dura lo suyo
-            if(self.phases == 4):
-                pass
+            #Si fase es 4, cada etapa dura lo suyo, verde, rojo, amarillo 1, amarillo 2 
+            if(self.phases != 3 or step < 4):
+                tl_dict[id].append(traci.trafficlight.Phase(x[i], LightsFunctions.phases[step]))    # 0, 1, 2 o 3 si phases == 4 
 
+        priority_phases = {"GrGr": 0, "rGrG": 2, "yryr": 1, "ryry": 3}
         for key in tl_dict:
-            traci.trafficlight.setProgramLogic(key, traci.trafficlight.Logic(key, 0, 0, tl_dict[key]))
+            tl_dict[key].sort(key=lambda x: priority_phases[x.state])
+            #traci.trafficlight.setProgramLogic(key, traci.trafficlight.Logic(key, 0, 0, tl_dict[key]))
 
-        #SUMO.run_simulation(self.simulationSteps)
+        SUMO.run_simulation(self.simulationSteps)
+
         SUMO.end_simulation()
-        return 0
+        return tl_dict
 
     # Recibe los tiempos y configura los semáforos en base a calles en lugar de aleatoriamente (Luego veo XD)
     def coordinated_light(self, x):
@@ -121,11 +130,22 @@ def probar():
     print(ended)
     SUMO.end_simulation()
 
+
+
 if __name__ == "__main__":
     #probar()
 
-    LF = LightsFunctions(250, True, 2)  #250 ciclos, Colores juntos, 2 tiempos
-    LF.all_lights(1)    #X temporal, luego la pongo bien XD
+    #'''
+    LF = LightsFunctions(250, False, 3)  #250 ciclos, Colores juntos, 2 tiempos
+    a = [i for i in range(1000)]
+    b = LF.all_lights(a)    #X temporal, luego la pongo bien XD
+
+    for i in b:
+        print(i, " ", end="")
+        for j in b[i]:
+            print(j.state, j.duration, end=" ")
+        print()
+        #'''
     
 
     
