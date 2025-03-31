6561f7dc144295ed35af177bce13fe0c0f61bb3d
Yael <yael.rodriguez@iteso.mx>
2025-03-30 19:06:40 -0600

    Ya estuvo

--- src/main.py
+++ src/main.py
@@ -5,48 +5,55 @@ from optimization import random_simulation, hill_simulation, swarm_simulation, g
 from data_writer import DataWriter
 import os
 
+STEPS = 200
 
-VISUAL = True
-
-def generate_files():
-    net = SumoSimulation.net_from_nod_edg("./assets/nodes.nod.xml", "./assets/edges.edg.xml", "./assets")
-    trips = SumoSimulation.random_trips(net, "./randomTrips.py", "./assets")
-    rou = SumoSimulation.rou_from_trip(net, trips, "./assets")
-    config = SumoSimulation.config_from_net_rou(os.path.basename(net), os.path.basename(rou), "./assets")
-    return config
-
-def generate_files_2(in_d, out_d):
+### Red, Entrada de tráfico, Salida de tráfico y tiempo
+def generate_files(net, in_d, out_d, time):
+    ### Genera las matrices de tráfico in_place en base a los diccionarios dados
     traffic = TrafficDemand.traffic_demand(in_d, out_d)
-    taz, od = TrafficDemand.write_taz_od(in_d, out_d, traffic, "0.0 0.01", "./assets")
-    print(taz, od)
-    net = SumoSimulation.net_from_nod_edg("./assets/nodes.nod.xml", "./assets/edges.edg.xml", "./assets")
+    ### Genera los archivos de la matriz de demanda (.taz y .od)
+    taz, od = TrafficDemand.write_taz_od(in_d, out_d, traffic, time, "./assets")
+    print()
+    ### Genera los viajes en base a la matriz (.trip)
     trips = SumoSimulation.trip_from_od(taz, od, "./assets")
+    ### Genera las rutas en base a los viajes (.rou)
     rou = SumoSimulation.rou_from_trip(net, trips, "./assets")
+    print(rou)
+    ### Genera el archivo de configuración en base a las rutas y red (.config)
     config = SumoSimulation.config_from_net_rou(os.path.basename(net), os.path.basename(rou), "./assets")
+
+    ###Retorna el archivo de configuración necesario para la simualaciones
     return config
 
-def test_simulation(config, steps = 250) -> dict[str, float]:
+### Corre la simulación default sin modificaciones
+def test_simulation(config, steps = STEPS) -> dict[str, float]:
     sumo = SumoSimulation(config)
-    sumo.start_simulation(VISUAL)
+    sumo.start_simulation(True)
     result = sumo.run_simulation(steps)
     sumo.end_simulation()
     return result
 
-def optimice_trafficlights(data = None) -> None:
+### Optimiza los semáforos y escribe los resultados 
+def optimice_trafficlights(config, data = None) -> None:
+    ### Objeto que escribe los resultados
     data_writer = DataWriter('default','data')
-    lights_function = LightsFunctions("./assets/simulation.sumocfg", steps=200, data_writer=data_writer)
+    ### Encargado de manejar la simulación y devolver resultados
+    lights_function = LightsFunctions(config, steps=STEPS, data_writer=data_writer)
+    ### Genera las listas mínimas y máximas de tiempo en base a la simulación () 
     x_low, x_high = lights_function.get_min_max(0, 150, 0, 5)
 
+    ### Correr y optimizar la simulación en base a los 3 algoritmos
     data_writer.change_file('random')
-    x1, y1 = random_simulation(lights_function.all_lights, x_low, x_high, 100, data["random"] if data else None)
+    random_simulation(lights_function.all_lights, x_low, x_high, 20, data["random"] if data else None)
     data_writer.change_file('hill')
-    x2, y2 = hill_simulation(lights_function.all_lights, x_low, x_high, 100, data["hill"] if data else None)
+    hill_simulation(lights_function.all_lights, x_low, x_high, 20, data["hill"] if data else None)
     data_writer.change_file('swarm')
-    s = swarm_simulation(lights_function.all_lights, x_low, x_high, 5, 20, data["swarm"] if data else None)
+    swarm_simulation(lights_function.all_lights, x_low, x_high, 2, 10, data["swarm"] if data else None)
     data_writer.change_file('genetic')
-    a = genetic_simulation(lights_function.all_lights, x_low, x_high, 5, 20, -5, 105)
+    genetic_simulation(lights_function.all_lights, x_low, x_high, 2, 10, -5, 105)
     data_writer.write_file()
 
+# Extrae las mejores configuraciones de los archivos y los devuelve como diccionario 
 def check_data():
     data_writer = DataWriter('default','data')  
     data = {}
@@ -69,10 +76,11 @@ def check_data():
 
     return data
 
-def show_cases(data):
-    simulation = LightsFunctions("./assets/simulation.sumocfg", 200)
+# En base a las mejores configuraciones, corre visualmente cada una de estas
+def show_cases(config, data):
+    simulation = LightsFunctions(config, 200)
 
-    test_simulation("./assets/simulation.sumocfg", 200)
+    test_simulation(config, 200)
 
     print("random")
     simulation.all_lights(data['random']['x'], True)
@@ -86,24 +94,25 @@ def show_cases(data):
     print("genetic")
     simulation.all_lights(data['genetic']['x'], True)
 
-    
-
 
 if __name__ == "__main__":
-    # Cambiar al directorio del script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
 
+    # Se puede reemplazar con lectura de archivo o similar
+    in_traffic = {"AD": 50}
+    out_traffic = {"IL": 50}
+    time = "0.0 0.01"
+    network = "./assets/network.net.xml"
 
-    generate_files_2({"AD": 50}, {"IL": 50})
-    #optimice_trafficlights()
+    configuration = generate_files(network, in_traffic, out_traffic, time)
+    #optimice_trafficlights(configuration)
     #data = check_data()
-    #show_cases(data)
+    #show_cases(configuration, data)
+
 
 
     
-
-
 
 
 
